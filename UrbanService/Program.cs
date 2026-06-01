32b967515185f8056fb36bbbec238e361064343d
OneMind-Dev <anhkvse182347@fpt.edu.vn>
2026-06-01 12:11:31 +0700

    Add VPS Docker image deployment workflow

--- UrbanService/Program.cs
+++ UrbanService/Program.cs
@@ -13,7 +13,6 @@ using UrbanService.Middlewares;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.WebHost.UseUrls("http://0.0.0.0:8080");
 // Add services to the container.
 
 builder.Services.AddControllers();
